b831081322bddfb620474ad3a3262b99e3c7757b
DavidLugo847 <krt847@gmail.com>
2021-11-23 17:15:18 -0500

    Agregando Modelo Vista-Controlador

--- CalculoIMC/app.js
+++ CalculoIMC/app.js
@@ -16,34 +16,19 @@ Servidor.get("/", (req, resp) => {
 
 
 Servidor.post("/CIMC", (req, resp) => {
-    let nomb = req.body.nombre;
+    let nomb = parseInt(req.body.nombre);
     let p = parseInt(req.body.peso);
-    let a = parseFloat(req.body.estatura);
+    let a = parseInt(req.body.altura);
 
     let res = p / (a * a);
-    if (res < 18.5) {
-        resp.send("Hola " + nomb + ".<br> Su indice de masa corporal es: " + res + ". <br><br> Estas por debajo del peso.");
+    if (res)
+        resp.send("Hola " + nombre + ".<br> Su indice de masa corporal es: " + res + ". <br>")
 
-    } else if (res >= 18.5 && res < 24.9) {
-        resp.send("Hola " + nomb + ".<br> Su indice de masa corporal es: " + res + ". <br><br>Estas Saludable😎");
-
-    } else if (res >= 25.0 && res < 29.9) {
-        resp.send("Hola " + nomb + ".<br> Su indice de masa corporal es: " + res + ". <br><br>Tienes Sobrepeso😐");
-    } else if (res >= 30.0 && res < 39.9) {
-        resp.send("Hola " + nomb + ".<br> Su indice de masa corporal es: " + res + ". <br><br>Tienes Obesidad☹");
-
-    } else if (res >= 40) {
-        resp.send("Hola " + nomb + ".<br> Su indice de masa corporal es: " + res + ". <br>Tienes Obesidad Morbida😖");
-
-    } else {
-        resp.send("Por favor introduce un dato válido.");
-
-    }
 });
 
 
 
 
 Servidor.listen(2000, () => {
-    console.log("Server activo 2000");
+    console.log("Server activo 3000");
 });
